5733650f5ecd2da15c6a44f68bb52f1e7d13d3e3
Stepahn Preeker <stephan@preeker.net>
2021-04-27 14:12:59 +0200

    fix: colom test

--- custom.go
+++ custom.go
@@ -19,7 +19,7 @@ func reduceWEQ(items Items) map[string]string {
 	result := make(map[string]string)
 	weq := 0
 	for i := range items {
-		_weq, err := strconv.ParseInt(items[i].Woningequivalent, 10, 64)
+		_weq, err := strconv.ParseInt(Woningequivalent.GetValue(items[i].Woningequivalent), 10, 64)
 		if err != nil {
 			panic(err)
 		}
