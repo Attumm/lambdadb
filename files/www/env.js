1d69fb1c504cb9d8efd72e6cdd88233acc792508
Attumm <bijman.m.m@gmail.com>
2021-05-14 22:10:23 +0200

    Added new fe, with multi sort support

--- files/www/env.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-param-reassign */
-/* eslint-disable no-underscore-dangle */
-// eslint-disable-next-line func-names
-(function(window) {
-  window.__env = window.__env || {};
-  window.__env.apiDomain = "";
-})(this);
